f36ca4027db8bc8bd8b6f6e178b8bc61867f23c8
Koustav17 <koustav33@gmail.com>
2024-12-02 22:57:00 +0530

    Update & Create API Modification

--- controller/food.js
+++ controller/food.js
@@ -3,10 +3,18 @@ var foodRouter = express.Router();
 const FoodSchema = require('../model/foodSchema');
 
 foodRouter.post('/createOrder', async (req, res) => {
+    var RandomString = Math.floor(Math.random() * 100) + 1;
+    var FinalString = 'HGFD' + RandomString.toString() + 'JHI';
     let payload = {
-        OrderId: req.body.OrderId,
+        OrderId: FinalString,
         OrderName: req.body.OrderName,
         FoodName: req.body.FoodName
+    }
+    if(req.body.OrderName.length < 3) {
+        return res.status(400).json({message: 'OrderName Should Not be Less than 3'});
+    }
+    if(req.body.FoodName.length < 3) {
+        return res.status(400).json({message: 'FoodName Should Not be Less than 3'});
     }
     await FoodSchema.create(payload).then((data) => {
         if (data) {
@@ -39,9 +47,14 @@ foodRouter.post('/deleteOrder/:id', async (req, res) => {
 
 foodRouter.post('/updateOrder/:id', async (req, res) => {
     let payload = {
-        OrderId: req.body.OrderId,
         OrderName: req.body.OrderName,
         FoodName: req.body.FoodName
+    }
+    if(req.body.OrderName.length < 3) {
+        return res.status(400).json({message: 'OrderName Should Not be Less than 3'});
+    }
+    if(req.body.FoodName.length < 3) {
+        return res.status(400).json({message: 'FoodName Should Not be Less than 3'});
     }
     await FoodSchema.findByIdAndUpdate({ _id: req.params.id }, payload).then((data) => {
         if (data) {

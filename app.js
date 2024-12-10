12b10cd3dd47954cd86e2092253c6eb28cab16e6
Koustav17 <koustav33@gmail.com>
2024-12-10 22:41:49 +0530

    File Path Updated Done

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const FoodRouter = require('./controller/food');
 const connectDB = require('./model/connectDB');
+const path = require('path');
 var app = express();
+
+app.get('/upload/:filename', (req, res) => {
+    res.sendFile(path.join(__dirname + '/upload/' +
+    req.params.filename));
+})
 
 app.use(express.json());
 app.use(FoodRouter);
+
 
 module.exports = app;
